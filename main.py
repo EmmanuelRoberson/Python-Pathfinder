cdca19cfd7673268bde9c3d0f316cf40e4a4ebe4
emmanuel <emmanuel.roberson3@gmail.com>
2020-12-14 17:42:25 -0600

    Added a waypoint class, added find_path function
    Waypoint class serves as a destination point that holds positional values that are used for pathfinding

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
-import pygame, sys
+import waypoint
+
+#Finds a target by searching through every possible node in the vicinity. Also known as Breadth First Search
+def find_path(starting_waypoint, target_waypoint):
+    open_list = [starting_waypoint] #add the start to the open list. open list is the list of waypoints that need to be checked
+    visited_list = [] #list of already visited waypoints
+
+    for cw in open_list: #iterate through all the waypoints in the open list
+
+        if (visited_list.__contains__(cw)): #if we already visited this waypoint, move to the next one
+            continue
+
+        visited_list.append(cw) #add the current neightbor to the visited list
+
+        if (cw is target_waypoint): #check if the current waypoint is the target
+            return cw #return the waypoint if true
+
+        current_neighbors = cw.get_neighbors() #get the neighbors of the current waypoint
+        
+        for cn in current_neighbors: #set each neighbors' parent to the current waypoint
+            cn.parent = cw
+
+        open_list.append(current_neighbors) #add the neighbors to the open list
+
+
+    return None #return none if the target isnt found
+
 
 def main():
-    pygame.init()
 
-    DISPLAY = pygame.display.set_mode((500,400),0, 23)
-
-    WHITE = (255,255,255)
-    BLUE = (0,0,255)
-
-    DISPLAY.fill(WHITE)
-
-    pygame.draw.rect(DISPLAY, BLUE, (200,150,100,50))
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == quit:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
+    #implementation goes here
+    return None
 
 
 main()
